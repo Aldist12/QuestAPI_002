70c4a9c2da2793f70cf5ea635734bcd047a16a65
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-12-30 01:41:24 +0700

    Menambahkan fungsi deleteMhs untuk menghapus data mahasiswa

--- app/src/main/java/com/example/project12/ui/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/project12/ui/viewModel/HomeViewModel.kt
@@ -1,15 +1,19 @@
 package com.example.project12.ui.viewModel
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.project12.model.Mahasiswa
 import com.example.project12.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+import kotlinx.serialization.InternalSerializationApi
 import okio.IOException
 import retrofit2.HttpException
 
 sealed class HomeUiState {
-    data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
+    data class Success @OptIn(InternalSerializationApi::class) constructor(val mahasiswa: List<Mahasiswa>) : HomeUiState()
     object Error : HomeUiState()
     object Loading : HomeUiState()
 }
@@ -21,6 +25,7 @@ class HomeViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
     init {
         getMhs()
     }
+    @OptIn(InternalSerializationApi::class)
     fun getMhs() {
         viewModelScope.launch {
             mhsUIState = HomeUiState.Loading
@@ -33,3 +38,15 @@ class HomeViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
             }
         }
     }
+    fun deleteMhs(nim: String) {
+        viewModelScope.launch {
+            try {
+                mhs.deleteMahasiswa(nim)
+            } catch (e: IOException) {
+                HomeUiState.Error
+            } catch (e: HttpException) {
+                HomeUiState.Error
+            }
+        }
+    }
+}
