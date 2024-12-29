8c26e26949ee3a151f9336c261f4c7e8aff677a9
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-12-30 01:47:18 +0700

    Mengimplementasikan error handling saat insert data mahasiswa

--- app/src/main/java/com/example/project12/ui/viewModel/InsertViewModel.kt
+++ app/src/main/java/com/example/project12/ui/viewModel/InsertViewModel.kt
@@ -1,11 +1,14 @@
 package com.example.project12.ui.viewModel
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.project12.model.Mahasiswa
 import com.example.project12.repository.MahasiswaRepository
 import kotlinx.coroutines.launch
+import kotlinx.serialization.InternalSerializationApi
 
 class InsertViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
     var uiState by mutableStateOf(InsertUiState())
@@ -17,6 +20,7 @@ class InsertViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
     }
 
 
+    @OptIn(InternalSerializationApi::class)
     suspend fun insertMhs() {
         viewModelScope.launch {
             try {
@@ -41,7 +45,23 @@ data class InsertUiEvent(
     val angkatan: String = "",
 )
 
+@OptIn(InternalSerializationApi::class)
 fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+@OptIn(InternalSerializationApi::class)
+fun Mahasiswa.toUiStateMhs(): InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent()
+)
+
+@OptIn(InternalSerializationApi::class)
+fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
     nim = nim,
     nama = nama,
     alamat = alamat,
