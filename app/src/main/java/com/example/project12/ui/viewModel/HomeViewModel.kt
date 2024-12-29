aa9a6e9e70f6fc1b985010c6c784b91327dfd7f0
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-12-30 01:38:45 +0700

    Menambahkan class HomeViewModel untuk halaman home

--- app/src/main/java/com/example/project12/ui/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/project12/ui/viewModel/HomeViewModel.kt
@@ -1,9 +1,20 @@
 package com.example.project12.ui.viewModel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
 import com.example.project12.model.Mahasiswa
+import com.example.project12.repository.MahasiswaRepository
 
 sealed class HomeUiState {
     data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
     object Error : HomeUiState()
     object Loading : HomeUiState()
 }
+
+class HomeViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
+    var mhsUIState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getMhs()
+    }
