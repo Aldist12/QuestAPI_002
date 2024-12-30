9db470cc4c8d016ebd9a328b87dcc0748eafbb03
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-12-30 14:26:45 +0700

    Menambahkan class UpdateViewModel untuk mengelola data mahasiswa saat update

---
+++ app/src/main/java/com/example/project12/ui/viewModel/UpdateViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.project12.ui.viewModel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.project12.repository.MahasiswaRepository
+import com.example.project12.ui.view.DestinasiUpdate
+import kotlinx.coroutines.launch
+import kotlinx.serialization.InternalSerializationApi
+
+
+@OptIn(InternalSerializationApi::class)
+class UpdateViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val mahasiswaRepository: MahasiswaRepository
+) : ViewModel() {
+    var updateUiState by mutableStateOf(InsertUiState())
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+
+    init {
+        viewModelScope.launch {
+            updateUiState = mahasiswaRepository.getMahasiswaById(_nim)
+                .toUiStateMhs()
+        }
+    }
+
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent) {
+        updateUiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+}
