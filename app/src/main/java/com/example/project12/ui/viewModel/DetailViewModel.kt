39f8f310d275286e1378c3d00f157f1f04e74e89
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-12-30 14:21:20 +0700

    Menambahkan DetailUiState untuk mengelola status UI detail mahasiswa

--- app/src/main/java/com/example/project12/ui/viewModel/DetailViewModel.kt
+++ app/src/main/java/com/example/project12/ui/viewModel/DetailViewModel.kt
@@ -31,3 +31,13 @@ class DetailViewModel(
         }
     }
 }
+
+data class DetailUiState(
+    val detailUiEvent: InsertUiEvent = InsertUiEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = "",
+) {
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != InsertUiEvent()
+}
